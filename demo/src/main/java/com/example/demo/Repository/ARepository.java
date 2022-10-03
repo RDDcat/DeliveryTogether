38a669cc00604c9e1848d59f05a091c5f02e0216
RDDcat <yukmaro@gmail.com>
2022-10-04 00:41:44 +0900

    [UPDATE] 리포지토리 업데이트

--- demo/src/main/java/com/example/demo/Repository/ARepository.java
+++ demo/src/main/java/com/example/demo/Repository/ARepository.java
@@ -1,9 +1,39 @@
 package com.example.demo.Repository;
 
+import com.example.demo.Model.DAO.PostDAO;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class ARepository {
 
+    private static Map<Long, PostDAO> store = new HashMap<>();
+
+    // 저장
+    public void save(PostDAO post){
+        store.put(post.getPostId(), post);
+    }
+
+    // id로 조회
+    public PostDAO loadById(Long id){
+        return store.get(id);
+    }
+
+    // 10개 찾기
+    public PostDAO[] load10(){
+        PostDAO[] postDAOS = new PostDAO[10];
+        int count = 0;
+
+        for (Long key : store.keySet()) {
+            postDAOS[count] = store.get(key);
+            if(++count == 10){
+                return postDAOS;
+            }
+        }
+
+        return postDAOS;
+    }
 
 }
