29c11228aee7c78f6cbe1045c74ca89f6767aa26
ECON <lgyoon621@gmail.com>
2022-11-29 18:37:39 +0900

    [UPDATE] Id 검색기능 추가

--- demo/src/main/java/com/example/demo/Repository/PostDAORepository.java
+++ demo/src/main/java/com/example/demo/Repository/PostDAORepository.java
@@ -1,7 +1,14 @@
 package com.example.demo.Repository;
 
 import com.example.demo.Model.DAO.PostDAO;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 public interface PostDAORepository extends JpaRepository<PostDAO, Long> {
+    @Override
+    Optional<PostDAO> findById(Long aLong);
 }
--- demo/src/main/java/com/example/demo/Repository/UserDAORepository.java
+++ demo/src/main/java/com/example/demo/Repository/UserDAORepository.java
@@ -3,5 +3,9 @@ package com.example.demo.Repository;
 import com.example.demo.Model.DAO.UserDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserDAORepository extends JpaRepository<UserDAO, Long> {
+    @Override
+    Optional<UserDAO> findById(Long aLong);
 }
