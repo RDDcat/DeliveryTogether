a17a4afca360d1d9903f113a9c53bc95f5346f14
ECON <lgyoon621@gmail.com>
2022-11-30 13:13:05 +0900

    [UPDATE] PostDAORepositoryCustom에 태그 검색 메소드 추가

--- demo/src/main/java/com/example/demo/Repository/PostDAORepositoryCustom.java
+++ demo/src/main/java/com/example/demo/Repository/PostDAORepositoryCustom.java
@@ -1,9 +1,9 @@
 package com.example.demo.Repository;
 
-import com.example.demo.Model.DTO.PostDTO;
+import com.example.demo.Model.DTO.PostTagSearchDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PostDAORepositoryCustom {
-    Page<PostDTO> searchPost(String tagname, Pageable pageable);
+    Page<PostTagSearchDTO> searchPost(String tagname, Pageable pageable);
 }
