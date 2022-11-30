fcffaa777d2ecb2517a6cfa2adafcff9397a5d7b
ECON <lgyoon621@gmail.com>
2022-11-30 10:04:18 +0900

    [UPDATE] UserDAORepository 조회 메소드 추가

--- demo/src/main/java/com/example/demo/Repository/UserDAORepository.java
+++ demo/src/main/java/com/example/demo/Repository/UserDAORepository.java
@@ -2,10 +2,16 @@ package com.example.demo.Repository;
 
 import com.example.demo.Model.DAO.UserDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface UserDAORepository extends JpaRepository<UserDAO, Long> {
     @Override
     Optional<UserDAO> findById(Long aLong);
+
+    Optional<UserDAO> findByEmail(String email);
+
+    UserDAO findByName(String nickname);
+
 }
