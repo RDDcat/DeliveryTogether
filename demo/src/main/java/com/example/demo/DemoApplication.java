f5c2eaf8adadc8cf15d95548b1ec17c895fb8741
ECON <lgyoon621@gmail.com>
2022-11-30 13:37:34 +0900

    [UPDATE] QueryDsl 빈으로 등록 및 데이터 베이스 연동

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,23 @@
 package com.example.demo;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+import javax.persistence.EntityManager;
+
+@SpringBootApplication
+//		(exclude = {DataSourceAutoConfiguration.class })
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
+	}
 }
