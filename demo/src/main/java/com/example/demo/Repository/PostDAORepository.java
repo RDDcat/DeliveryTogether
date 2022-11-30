6a16b9ee8222dacb8470e25bce067bb6ee9b5796
ECON <lgyoon621@gmail.com>
2022-11-30 17:00:57 +0900

    [UPDATE] PostDAORepository findAll 추가

--- demo/src/main/java/com/example/demo/Repository/PostDAORepository.java
+++ demo/src/main/java/com/example/demo/Repository/PostDAORepository.java
@@ -1,14 +1,18 @@
 package com.example.demo.Repository;
 
 import com.example.demo.Model.DAO.PostDAO;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
 public interface PostDAORepository extends JpaRepository<PostDAO, Long>, PostDAORepositoryCustom {
     @Override
     Optional<PostDAO> findById(Long aLong);
+
+    List<PostDAO> findAllByPostTagDAOS(String tagName);
+
+    @Override
+    List<PostDAO> findAll();
 }
