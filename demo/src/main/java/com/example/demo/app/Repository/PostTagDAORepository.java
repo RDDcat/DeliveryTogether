c701ee0b5c746f9839ff827579eb509ba4836cf9
ECON <lgyoon621@naver.com>
2022-12-06 00:04:54 +0900

    [UPDATE] findBy 메소드 추가

--- demo/src/main/java/com/example/demo/app/Repository/PostTagDAORepository.java
+++ demo/src/main/java/com/example/demo/app/Repository/PostTagDAORepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.app.Repository;
 
+import com.example.demo.app.Model.DAO.PostDAO;
 import com.example.demo.app.Model.DAO.PostTagDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostTagDAORepository extends JpaRepository<PostTagDAO,Long> {
+    Optional<PostTagDAO> findByPostDAO(PostDAO postDAO);
 }
--- demo/src/main/java/com/example/demo/app/Repository/UserTagDAORepository.java
+++ demo/src/main/java/com/example/demo/app/Repository/UserTagDAORepository.java
@@ -3,5 +3,10 @@ package com.example.demo.app.Repository;
 import com.example.demo.app.Model.DAO.UserTagDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserTagDAORepository extends JpaRepository<UserTagDAO, Long> {
+
+    Optional<UserTagDAO> findByUserDAO(UserTagDAO userTagDAO);
+
 }
