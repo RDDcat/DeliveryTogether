f2e02f6ccbbc03e849632937d1d3a14ea93502ef
ECON <lgyoon621@naver.com>
2022-12-06 18:22:39 +0900

    [UPDATE] findById, deleteById 추가

--- demo/src/main/java/com/example/demo/Repository/UserTagDAORepository.java
+++ demo/src/main/java/com/example/demo/Repository/UserTagDAORepository.java
@@ -6,7 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserTagDAORepository extends JpaRepository<UserTagDAO, Long> {
 
+    @Override
+    Optional<UserTagDAO> findById(Long id);
+
+    @Override
+    void deleteById(Long aLong);
 }
