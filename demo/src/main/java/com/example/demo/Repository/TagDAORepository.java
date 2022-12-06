40e6d38008470380429509970bf88216b1cb523b
ECON <lgyoon621@naver.com>
2022-12-06 18:47:45 +0900

    [UPDATE] 유저 태그 생성 삭제 기능 추가

--- demo/src/main/java/com/example/demo/Repository/TagDAORepository.java
+++ demo/src/main/java/com/example/demo/Repository/TagDAORepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TagDAORepository extends JpaRepository<TagDAO, Long> {
-    Optional<TagDAO> findByName(String name);
+    Optional<TagDAO> findByName(String tagName);
 }
--- demo/src/main/java/com/example/demo/Service/UserService.java
+++ demo/src/main/java/com/example/demo/Service/UserService.java
@@ -1,18 +1,64 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.DAO.TagDAO;
 import com.example.demo.Model.DAO.UserDAO;
+import com.example.demo.Model.DAO.UserTagDAO;
+import com.example.demo.Repository.TagDAORepository;
 import com.example.demo.Repository.UserDAORepository;
+import com.example.demo.Repository.UserTagDAORepository;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 public class UserService {
     private final UserDAORepository userDAORepository;
 
+    private final UserTagDAORepository userTagDAORepository;
 
+    private final TagDAORepository tagDAORepository;
 
     public Optional<UserDAO> loadUserById(Long userId){
         return userDAORepository.findById(userId);
     }
+
+    //삭제
+    public void deleteTags(List<Long> ids){
+        if(ids.size() > 0){
+            for (Long id : ids) {
+                userTagDAORepository.deleteById(id);
+            }
+        }
+    }
+
+    //수정
+    public void addTags(Long userId, List<String> tagNames){
+        for (String tagName : tagNames) {
+
+            Optional<TagDAO> getTag = tagDAORepository.findByName(tagName);
+            TagDAO tag = null;
+
+            // 태그가 등록되어 있지 않을 때
+            if(getTag == null){
+                tag = tagDAORepository.save(TagDAO.builder()
+                        .name(tagName)
+                        .build());
+            }
+            // 태그가 있을 때
+            else {
+                tag = getTag.get();
+            }
+
+            UserTagDAO userTag = UserTagDAO.builder()
+                    .name(tag.getName())
+                    .userDAO(userDAORepository.findById(userId).get())
+                    .tagDAO(tag)
+                    .build();
+
+            userTagDAORepository.save(userTag);
+        }
+    }
+
+
 }
