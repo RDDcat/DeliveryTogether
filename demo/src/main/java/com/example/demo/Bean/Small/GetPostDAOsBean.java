da91ccded7c94c1283a32e8c9e7b34af619fe36d
ECON <lgyoon621@naver.com>
2022-12-06 18:25:12 +0900

    [UPDATE] GetPostDAOsBean에서 Page<PostTagSearchDTO>로 리턴 타입을 변경하고 함수 변경

--- demo/src/main/java/com/example/demo/Bean/Small/GetPostDAOsBean.java
+++ demo/src/main/java/com/example/demo/Bean/Small/GetPostDAOsBean.java
@@ -1,9 +1,12 @@
 package com.example.demo.Bean.Small;
 
 import com.example.demo.Model.DAO.PostDAO;
+import com.example.demo.Model.DTO.PostTagSearchDTO;
 import com.example.demo.Repository.ARepository;
 import com.example.demo.Repository.PostDAORepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,9 +16,14 @@ public class GetPostDAOsBean {
     @Autowired
     PostDAORepository repository;
 
-    public List<PostDAO> exec(String tagName){
+    //TODO Page<PostTagSearchDTO>로 리턴타입을 변경해서 이거에 해당하는 것들 수정 필요
+    //findAllByPostDAOs 연관관계 오류로 인해서 삭제함 수정 필요
+    public Page<PostTagSearchDTO> exec(String tagName, int page, int size){
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+
         // 위임
-        return repository.findAllByPostTagDAOS(tagName);
+        return repository.searchPost(tagName, pageRequest);
     }
 
     public PostDAO[] exec(List<String> tagNames) {
