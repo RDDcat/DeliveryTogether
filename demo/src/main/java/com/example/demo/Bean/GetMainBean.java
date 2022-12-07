2630066dbb6b07d3ac79a821c98f1f9b81bd2c35
RDDcat <yukmaro@gmail.com>
2022-12-07 20:56:11 +0900

    [UPDATE] 메인페이지 업데이트

--- demo/src/main/java/com/example/demo/Bean/GetMainBean.java
+++ demo/src/main/java/com/example/demo/Bean/GetMainBean.java
@@ -25,19 +25,33 @@ public class GetMainBean {
     GetUserIdBean getUserIdBean;
 
     public MainPageDTO exec(String token){
+
+        System.out.println("토큰 중간 확인 : " + token);
         // 토큰으로 유저 아이디 가져오기
         Optional<UserDAO> userDAO = getUserIdBean.exec(token);
-        long userId = userDAO.get().getUserId();
+        System.out.println("토큰 중간 확인 : " + token);
+
+        long userId = 1;
+
+        //
+        if(userDAO.isPresent()){
+            userDAO.get().getUserId();
+        }
+
 
         // 사용자 아이디로 사용자가 구독한 태그 이름 가져오기
         List<String> tagNames = getTagsBean.exec(userId);
+        System.out.println("토큰 중간 확인 : " + token);
+
 
         // 태그 이름으로 저장소에서 PostDAO 가져오기
         // TODO 함수 리턴 형식 변경으로 인한 변경 필요할거 같음
         List<MetaPostDTO> metaPostDTOS = getPostDAOsBean.exec(tagNames);
 
         // DAO -> DTO 변환 후 리턴
-        MainPageDTO mainPageDTO = getMainPageDTOBean.exec(metaPostDTOS, userId);
+        MainPageDTO mainPageDTO = getMainPageDTOBean.exec(metaPostDTOS, userId, token);
+
+        System.out.println("토큰 중간 확인 : " + token);
         
         return mainPageDTO;
     }
